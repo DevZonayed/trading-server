3a50acd40eec16b76cbe7ac28f1315c34fa3894b
Zonayed Ahamad <95507109+DevZonayed@users.noreply.github.com>
2023-09-14 16:33:30 +0600

    remove bcrypt

--- index.js
+++ index.js
@@ -4,21 +4,10 @@ const mongoConnection = require("./api/config/databaseConfig");
 require("dotenv").config();
 const cors = require("cors");
 const compression = require("compression");
-const cookieParser = require("cookie-parser");
 const {
   errorMiddleware,
   notFoundMiddleware,
 } = require("./api/middleware/error/errorMiddleware");
-const userRouter = require("./api/router/UserRouter");
-const subjectRouter = require("./api/router/SubjectRouter");
-const sessionRouter = require("./api/router/SessionRouter");
-const emailTemplateRouter = require("./api/router/EmailTemplateRouter");
-const SettingsRouter = require("./api/router/SettingsRouter");
-const LeadRouter = require("./api/router/LeadRouter");
-const messageRouter = require("./api/router/messageRouter");
-const mailRouter = require("./api/router/mailRouter");
-const dashBordRouter = require("./api/router/dashBordRouter");
-const generalRouter = require("./api/router/generalRouter");
 const scheduleJobs = require("./api/schedules/scheduleJob");
 
 const path = require("path");
@@ -53,19 +42,6 @@ app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("trust proxy", true);
-// app.use(cookieParser());
-// User Router
-// app.use("/api/v1/user", userRouter);
-// app.use("/api/v1/lead", LeadRouter);
-// app.use("/api/v1/subject", subjectRouter);
-// app.use("/api/v1/session", sessionRouter);
-// app.use("/api/v1/template", emailTemplateRouter);
-// app.use("/api/v1/settings", SettingsRouter);
-// app.use("/api/v1/message", messageRouter);
-// app.use("/api/v1/mail", mailRouter);
-// app.use("/api/v1/dashbord", dashBordRouter);
-// app.use("/api/v1/general", generalRouter);
-// app.use("/api/v1/trading-server", generalRouter);
 
 app.get("/", (req, res) => {
   res.json({
@@ -74,8 +50,8 @@ app.get("/", (req, res) => {
 });
 
 // Error Middleware
-// app.use(notFoundMiddleware);
-// app.use(errorMiddleware);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const PORT = process.env.SERVER_PORT || 7000;
 mongoConnection();
