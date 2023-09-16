526f186f459c27d02254e5f3da4e40d9933f9490
Zonayed Ahamad <95507109+DevZonayed@users.noreply.github.com>
2023-09-16 12:19:50 +0600

    trading hook added

--- api/router/tradingDataRoutes.js
+++ api/router/tradingDataRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const tradingDataController = require("../controllers/tradingDataController");
+const tradingDataController = require("../controller/tradingDataController");
 
 // Define your routes here
 router.post("/", tradingDataController.createTradingData);
