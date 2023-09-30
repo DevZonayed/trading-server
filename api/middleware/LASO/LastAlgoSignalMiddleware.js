66dc80bffb0da7d3085b8140ca48a1deebea7b5d
Zonayed Ahamad <95507109+DevZonayed@users.noreply.github.com>
2023-09-30 23:58:54 +0600

    LastAlgo Telegram notification added

---
+++ api/middleware/LASO/LastAlgoSignalMiddleware.js
@@ -0,0 +1,37 @@
+const AsyncHandler = require("express-async-handler");
+const { csvStringToObject } = require("../../helper/phoneNumberFormater");
+const { TelegramInstance } = require("../../service/Telegram");
+
+const LastAlgoSignalMiddleware = AsyncHandler(async (req, res, next) => {
+  let bodyData = csvStringToObject(req.body)[0];
+  let { name, time, timeframe, type, ...restData } = bodyData;
+  if (type != "LASO1" || type != "LASO5" || !type) {
+    next();
+  }
+
+  // Construct the update query
+  const updateQuery = {
+    $set: {},
+  };
+
+  // Set the nested object in the data field based on the type key
+  updateQuery.$set[`data.${type}`] = restData;
+  const updatedDocument = await TradingData.findOneAndUpdate(
+    { name, time, timeframe },
+    updateQuery,
+    {
+      new: true,
+      upsert: true,
+    }
+  );
+
+  let message = generateNotify(updatedDocument.data[type]);
+  message =
+    `${timeframe} Candles ${type} data report:\n ==========================\n` +
+    message;
+  // Send repost to telegram
+  TelegramInstance.sendMessage(message);
+  res.status(200).json(updatedDocument);
+});
+
+module.exports = { LastAlgoSignalMiddleware };
---
+++ api/middleware/LASO/Notifications/genNotifyTel.js
@@ -0,0 +1,71 @@
+function generateNotify(allData) {
+  const {
+    bullish,
+    bullish_plus,
+    bearish,
+    bearish_plus,
+    bullish_exit,
+    bearish_exit,
+    trand_strength,
+    take_profit,
+    stop_loss,
+    bar_color_value,
+    trend_tracer,
+    trend_catcher,
+    smart_trail,
+    smart_trail_extremity,
+    rz_r3_band,
+    rz_r2_band,
+    rz_r1_band,
+    rz_s1_band,
+    rz_s2_band,
+    rz_s3_band,
+  } = allData;
+
+  message = "";
+  if (bullish !== "0" || !bullish) {
+    message = `Buy Signal Occered \n`;
+  }
+
+  if (bullish_plus !== "0" || !bullish_plus) {
+    message = `Strong Buy Signal Occered \n`;
+  }
+
+  if (bearish !== "0" || !bullish) {
+    message = `Sell Signal Occered \n`;
+  }
+
+  if (bearish_plus !== "0" || !bullish_plus) {
+    message = `Strong Sell Signal Occered \n`;
+  }
+
+  if (bearish_exit !== "null" || !bullish_exit) {
+    message = `Sell Exit Signal Occered \n`;
+  }
+
+  if (bullish_exit !== "null" || !bullish_exit) {
+    message = `Buy Exit Signal Occered \n`;
+  }
+
+  message = `Trand Strength is ${(+trand_strength).toFixed(2)}% \n`;
+  message = `Bar Color Is ${
+    +bar_color_value < 0 ? "RED" : +bar_color_value > 0 ? "GREEN" : "VIOLET"
+  } \n`;
+  message = `Dynamic Profit taking price should ${(+take_profit).toFixed(
+    2
+  )} \n`;
+  message = `Dynamic Stop Lose price should ${(+stop_loss).toFixed(2)} \n`;
+  message = `Trand Tracker IS ${(+trend_tracer).toFixed(2)} \n`;
+  message = `Trand Catcher IS ${(+trend_catcher).toFixed(2)} \n`;
+  message = `Smart Trail IS ${(+smart_trail).toFixed(2)} \n`;
+  message = `Smart Trail Extremity IS ${(+smart_trail_extremity).toFixed(
+    2
+  )} \n`;
+  message = `Rz r3 band IS ${(+rz_r3_band).toFixed(2)} \n`;
+  message = `Rz r2 band IS ${(+rz_r2_band).toFixed(2)} \n`;
+  message = `Rz r1 band IS ${(+rz_r1_band).toFixed(2)} \n`;
+  message = `Rz s1 band IS ${(+rz_s1_band).toFixed(2)} \n`;
+  message = `Rz s2 band IS ${(+rz_s2_band).toFixed(2)} \n`;
+  message = `Rz s3 band IS ${(+rz_s3_band).toFixed(2)} \n`;
+  return message;
+}
