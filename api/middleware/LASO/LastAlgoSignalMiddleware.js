848afb406a563880e14a79cf1d94643a9a52f2af
Zonayed Ahamad <95507109+DevZonayed@users.noreply.github.com>
2023-10-01 23:35:47 +0600

    Stretagy 1 Condition Applyed

--- api/middleware/LASO/LastAlgoSignalMiddleware.js
+++ api/middleware/LASO/LastAlgoSignalMiddleware.js
@@ -1,14 +1,18 @@
 const { csvStringToObject } = require("../../helper/phoneNumberFormater");
 const TradingData = require("../../model/TradingData");
-const { TelegramInstance } = require("../../service/Telegram");
+const {
+  TelegramInstance,
+  TelegramPandaBite5MinInstance,
+} = require("../../service/Telegram");
 const AsyncHandler = require("express-async-handler");
 const { generateNotify } = require("./Notifications/genNotifyTel");
+const organizeLaData = require("./helper/OrganizeLaData");
+const { signalStrengthStrategy } = require("./Strategy/SignalStrength");
 
 const LastAlgoSignalMiddleware = AsyncHandler(async (req, res, next) => {
   let bodyData = csvStringToObject(req.body)[0];
   let { name, time, timeframe, type, ...restData } = bodyData;
-  if (!(type == "LASO1" || type == "LASO5" || !type)) {
-    console.log("Called Next");
+  if (!(type == "LASO" || type == "LASO" || !type)) {
     return next();
   }
 
@@ -33,7 +37,37 @@ const LastAlgoSignalMiddleware = AsyncHandler(async (req, res, next) => {
     `<b><i>${timeframe} Candles ${type} data report:</i></b>\n ==========================\n` +
     message;
   // Send repost to telegram
-  TelegramInstance.sendMessagehtml(message);
+  // TelegramInstance.sendMessagehtml(message);
+  let candleData = await organizeLaData(updatedDocument, type);
+  TelegramInstance.sendMessagehtml(`
+  <pre>
+  ${timeframe} min Data Update
+  signal: ${candleData.signal},
+  isClose: ${candleData.isClose},
+  polished_trend_catcher: ${candleData.polished_trend_catcher},
+  take_profit: ${candleData.take_profit},
+  stop_loss: ${candleData.stop_loss},
+  trand_strength: ${candleData.trand_strength},
+  barColor: ${candleData.barColor},
+  smart_trail: ${candleData.smart_trail},
+  trend_tracer: ${candleData.trend_tracer},
+  upperTail: ${candleData.upperTail}%,
+  lowerTail: ${candleData.lowerTail}%
+  </pre>
+  `);
+
+  // Apply stratagy 1
+  let strengthStratgy = signalStrengthStrategy(candleData, updatedDocument);
+  if (strengthStratgy.status) {
+    TelegramPandaBite5MinInstance.sendMessage(
+      `
+      COIN*: ${strengthStratgy.data.coin}
+      Direction*: ${strengthStratgy.data.direction}
+      Exchange*: ${strengthStratgy.data.exchange}
+      ENTRY*: ${strengthStratgy.data.entry}
+      `
+    );
+  }
   res.status(200).json(updatedDocument);
 });
 
