754eb424c4e8a2e0954aea85ec572aa7ca44de39
Zonayed Ahamad <95507109+DevZonayed@users.noreply.github.com>
2023-11-06 17:24:41 +0600

    Feature Add: Dynamic Leverage

--- api/Stratagys/OneHourTortories/subStrategys/OneMinStr-UltraTickTrader/Settings.js
+++ api/Stratagys/OneHourTortories/subStrategys/OneMinStr-UltraTickTrader/Settings.js
@@ -11,7 +11,9 @@ const SETTINGS = {
         laverageType : "Isolated",
         minimumTrendStrength : 5,
         maximumTailSize : 0.15,
-        maximumCandleSize : 1
+        maximumCandleSize : 1,
+        minTrendStrength : 15,
+        defaultLeverage : 1
     },
     telegramCradentials: {
         botToken: "6465687056:AAFAoET1Ln3zRRutTy6nvlfT2FAeVvJscMI",
@@ -33,13 +35,20 @@ const SETTINGS = {
                 WtLbOpositeGreenLine : "WT_LB Greenline is oposite direction and it's greater then 40. \n",
                 MaxTailHapped : "The tail of the order candle if so over! \n",
                 MaximumBodySizeNoticed : "Candle low to high percentage is touched maximum candle size \n"
-                
             },
             execute : {
-
             },
             laverage : {
-
+                OneXAiChannelAiSuperTrendOposite : "Ai Channel and Super Trend Have Oposite Direction\N",
+                OneXLowTrendStrength : "Low Trend Strength \n",
+                OneXBatterMacdOpositeDirection : "Batter Macd Is in oposite direction\n",
+                OneXBarOposite : "Bar Color Is not in favour \n",
+                TwoXBatterMacdInSameDirection : "Batter Macd In the same direction\n",
+                TwoXAiInSameDirectionNotMacd : "Ai Channel And Ai SuperTrend has same direction of signals but not macd\n",
+                ThreeXAllConditionMeet : "Smart Trail , SuperTrend , Ai Channels , Macd ,  Strong bull/ bear \n All Directions in favour\n",
+                FiveXAllConditionMeet : "WT_LB Green Line Are Above 40 with same direction\n of the value of the green line of WT_LB \n",
+                SixXAllConditionMeet : "Smart Trail , Ai SuperTrend , Ai Channels , Macd , bar color , default candle direction \n All of the values is in favour\n",
+                DefaultLeverageMessage : "No Conditions has matched it take from default sourch\n"
             }
         }
     }
--- api/Stratagys/OneHourTortories/subStrategys/OneMinStr-UltraTickTrader/decisionMaker/OrderValidator.js
+++ api/Stratagys/OneHourTortories/subStrategys/OneMinStr-UltraTickTrader/decisionMaker/OrderValidator.js
@@ -47,7 +47,7 @@ async function HandleTradeOrder(candleData, prevCandles) {
  * This function will be responsible for execute trade
  * @param {direction , candleData} param0 
  */
-async function handleTrade({ direction, candleData  , prevCandles}) {
+async function handleTrade({ direction, candleData, prevCandles }) {
     let isFalseTrade = isTradeFalsifiable({ direction, candleData, prevCandles });
 
     if (isFalseTrade) {
@@ -58,7 +58,7 @@ async function handleTrade({ direction, candleData  , prevCandles}) {
         return false
     }
 
-    let processedLeverage = handleLeverage(candleData, prevCandles);
+    let processedLeverage = handleLeverage(direction, candleData, prevCandles);
 
     // Send the leverage alert
     telegram.sendMessage(`Trade Leverage Gonna be ${processedLeverage.value} \n Reson : ${processedLeverage.reson}`)
@@ -112,16 +112,352 @@ async function handleTradeExecute(payload) {
  * @param {prevCandleData[]} prevCandles 
  * @returns 
  */
-function handleLeverage(candleData, prevCandles) {
-    let baseLeverage = 1;
-    let reson = "There is no condition applyed it's default"
+function handleLeverage(direction, candleData, prevCandles) {
     // All Conditions Will Goes Here
+    let OneXTreaty = checkOneXLeverageTreaty(direction, candleData);
+    let TwoXTreaty = checkTwoXLeverageTreaty(direction, candleData);
+    let ThreeXTreaty = checkThreeXLeverageTreaty(direction, candleData);
+    let FiveXTreaty = checkFiveXTreaty(direction, candleData);
+    let SixXTreaty = checkSixXLeverageTreaty(direction, candleData);
+
+    let leverage = 1;
+    let reason = "";
+    function addCredentials({
+        leverage : _leverage,
+        reason : _reason
+    }) {
+        leverage = _leverage;
+        reason = _reason;
+    }
+
+    if(OneXTreaty.status){
+        addCredentials(OneXTreaty)
+    }else if(TwoXTreaty.status){
+        addCredentials(TwoXTreaty)
+    }else if(ThreeXTreaty.status){
+        addCredentials(ThreeXTreaty)
+    }else if(FiveXTreaty.status){
+        addCredentials(FiveXTreaty)
+    }else if(SixXTreaty.status){
+        addCredentials(SixXTreaty)
+    }else{
+        let defaultLeverage = {
+            leverage : SETTINGS.order.defaultLeverage,
+            reason : SETTINGS.MESSAGES.ORDER.laverage.DefaultLeverageMessage
+        }
+        addCredentials(defaultLeverage)
+    }
+
     return {
-        value: baseLeverage,
-        reson
+        value: leverage,
+        reson : reason
     };
 }
 
+/**
+ * This function will contain 1x leverage conditions
+ * @param {*} candleData 
+ * @returns {status : Boolean , leverage : Number , reason : string}
+ */
+function checkOneXLeverageTreaty(direction, candleData) {
+    const Leverage = 1;
+    let aiChannelDirection = determinAiChannelDirection(candleData);
+    let aiSuperTrandDirection = determinAiSuperTrendDirection(candleData);
+    let trendStrength = candleData.data.trendStrength;
+    let barDirection = determinBarColorDirection(candleData);
+    let macdDirection = determineBetterMACDdirection(candleData)
+
+    let reason = null;
+
+    // This function will help to append reasons
+    function setReason(message) {
+        if (!message) {
+            return false;
+        }
+        reason
+            ? reason += message
+            : reason = message;
+    }
+
+    if (aiChannelDirection !== aiSuperTrandDirection) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.OneXAiChannelAiSuperTrendOposite)
+    }
+    if (trendStrength && trendStrength < SETTINGS.order.minTrendStrength) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.OneXLowTrendStrength)
+
+    }
+    if (barDirection !== direction) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.OneXBarOposite)
+    }
+    if (direction !== macdDirection) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.OneXBatterMacdOpositeDirection)
+    }
+
+    return {
+        status: !!reason,
+        leverage: Leverage,
+        reason
+    };
+
+
+}
+
+
+/**
+ * This function will contain 2x leverage conditions
+ * @param {*} candleData 
+ * @returns {status : Boolean , leverage : Number , reason : string}
+ */
+function checkTwoXLeverageTreaty(direction, candleData) {
+    const Leverage = 2;
+    let aiChannelDirection = determinAiChannelDirection(candleData);
+    let aiSuperTrandDirection = determinAiSuperTrendDirection(candleData);
+    let macdDirection = determineBetterMACDdirection(candleData)
+
+    let reason = null;
+
+
+    // This function will help to append reasons
+    function setReason(message) {
+        if (!message) {
+            return false;
+        }
+        reason
+            ? reason += message
+            : reason = message;
+    }
+
+    if (direction == macdDirection) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.TwoXBatterMacdInSameDirection)
+    }
+
+    if (direction == aiChannelDirection && direction == aiSuperTrandDirection && direction !== macdDirection) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.TwoXAiInSameDirectionNotMacd)
+    }
+
+    return {
+        status: !!reason,
+        leverage: Leverage,
+        reason
+    }
+
+
+}
+
+/**
+ * This function will contain 2x leverage conditions
+ * @param {*} candleData 
+ * @returns {status : Boolean , leverage : Number , reason : string}
+ */
+function checkThreeXLeverageTreaty(direction, candleData) {
+    const Leverage = 3;
+    let aiChannelDirection = determinAiChannelDirection(candleData);
+    let aiSuperTrandDirection = determinAiSuperTrendDirection(candleData);
+    let aiSuperTrandStrongDirection = determinAiSuperTrendStrongDirection(candleData);
+    let macdDirection = determineBetterMACDdirection(candleData)
+    let smartTrailDirection = candleData?.data?.smartTrailStatus;
+    let reason = null;
+    // This function will help to append reasons
+    function setReason(message) {
+        if (!message) {
+            return false;
+        }
+        reason
+            ? reason += message
+            : reason = message;
+    }
+    if (
+        direction == smartTrailDirection
+        && direction == aiSuperTrandDirection
+        && direction == aiChannelDirection
+        && direction == macdDirection
+        && direction == aiSuperTrandStrongDirection
+    ) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.ThreeXAllConditionMeet)
+    }
+    return {
+        status: !!reason,
+        leverage: Leverage,
+        reason
+    }
+
+
+}
+
+/**
+ * This function will contain 5x leverage conditions
+ * @param {Candle Data} candleData 
+ * @returns {status : Boolean , leverage : Number , reason : string}
+ */
+function checkFiveXTreaty(direction, candleData) {
+    const Leverage = 5;
+    let WTGreenLineDirection = determinWTLBGreenLine(candleData)
+    let greenLine = Number(candleData?.data?.wtgl);
+
+    let reason = null;
+    // This function will help to append reasons
+    function setReason(message) {
+        if (!message) {
+            return false;
+        }
+        reason
+            ? reason += message
+            : reason = message;
+    }
+    if (!isNaN(greenLine) && Math.abs(greenLine) > 40 && direction == WTGreenLineDirection) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.FiveXAllConditionMeet)
+    }
+
+    return {
+        status: !!reason,
+        leverage: Leverage,
+        reason
+    }
+}
+
+
+function checkSixXLeverageTreaty(direction, candleData) {
+    const Leverage = 6;
+    let candleDirection = determindefaultCandleDirection(candleData)
+    let aiChannelDirection = determinAiChannelDirection(candleData);
+    let aiSuperTrandDirection = determinAiSuperTrendDirection(candleData);
+    let barDirection = determinBarColorDirection(candleData);
+    let macdDirection = determineBetterMACDdirection(candleData)
+    let aiSuperTrandStrongDirection = determinAiSuperTrendStrongDirection(candleData);
+    let smartTrailDirection = candleData.data.smartTrailStatus;
+
+
+    let reason = null;
+    // This function will help to append reasons
+    function setReason(message) {
+        if (!message) {
+            return false;
+        }
+        reason
+            ? reason += message
+            : reason = message;
+    }
+
+    if (
+        direction == smartTrailDirection &&
+        direction == aiSuperTrandDirection &&
+        direction == aiChannelDirection &&
+        direction == macdDirection &&
+        direction == barDirection &&
+        direction == candleDirection &&
+        direction == aiSuperTrandStrongDirection
+    ) {
+        setReason(SETTINGS.MESSAGES.ORDER.laverage.SixXAllConditionMeet)
+    }
+
+    return {
+        status: !!reason,
+        leverage: Leverage,
+        reason
+    }
+}
+
+/**
+ * This function will determine WTLB Green Line
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | false}
+ */
+function determinWTLBGreenLine(candleData) {
+    let greenLine = Number(candleData?.data?.wtgl);
+    if (isNaN(greenLine)) {
+        return false
+    }
+
+    let direction = greenLine > 0 ? "Long" : greenLine < 0 ? "Short" : false
+    return direction;
+}
+
+
+/**
+ * this function is for determine Ai Channels indicator direction
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | false}
+ */
+function determinAiChannelDirection(candleData) {
+    let upper = candleData?.data?.upper;
+    let lower = candleData?.data?.lower;
+    let direction = isNaN(lower) && !isNaN(upper) ? "Long" : isNaN(upper) && !isNaN(lower) ? "Short" : false;
+    return direction
+}
+
+
+/**
+ * this function is for determine Ai Super Trand indicator's direction
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | false}
+ */
+function determinAiSuperTrendDirection(candleData) {
+    let consensus = candleData?.data?.consensus;
+    if (isNaN(consensus)) {
+        return false;
+    }
+    let direction = +consensus > 0 ? "Long" : +consensus < 0 ? "Short" : false;
+    return direction
+}
+
+
+/**
+ * This function is for determine Ai Super Trand indicator's Strong direction
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | false}
+ */
+function determinAiSuperTrendStrongDirection(candleData) {
+    let strongBullish = Math.abs(candleData?.data?.strongBullish);
+    let strongBearish = Math.abs(candleData?.data?.strongBearish);
+    if (!strongBearish && !strongBullish) {
+        return false;
+    }
+
+    let direction = !!strongBullish ? "Long" : !!strongBearish ? "Short" : false;
+    return direction
+}
+
+
+function determindefaultCandleDirection(candleData) {
+    let candleStatus = candleData.candleStatus;
+    candleStatus = Number(candleStatus);
+    if (isNaN(candleStatus)) {
+        return false
+    }
+
+    let direction = candleStatus > 0 ? "Long" : candleStatus < 0 ? "Short" : false
+    return direction
+}
+
+
+/**
+ * This function will determin the trade direction based on candle color
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | "Neutral"}
+ */
+function determinBarColorDirection(candleData) {
+    let barColor = +candleData?.data?.candleColor;
+    if (isNaN(barColor)) {
+        return false;
+    }
+    let direction = barColor > 0 ? "Long" : barColor < 0 ? "Short" : "Neutral";
+    return direction
+}
+
+/**
+ * This function will determin the trade direction based on MACD Line
+ * @param {Candle Data} candleData 
+ * @returns {"Long" | "Short" | "Neutral"}
+ */
+function determineBetterMACDdirection(candleData) {
+    let macdLine = candleData?.data?.macdLine;
+    let signalLine = candleData?.data?.signalLine;
+    if (isNaN(macdLine) || isNaN(signalLine)) {
+        return false;
+    }
+    let direction = macdLine > signalLine ? "Long" : macdLine < signalLine ? "Short" : "Neutral";
+    return direction;
+}
 
 
 /**
@@ -198,7 +534,7 @@ function isTradeFalsifiable({ direction, candleData }) {
 async function handleTradeWithExistingTrade(isPrimaryTrade, trendCatcherShift, prevTrade, candleData, prevCandles) {
     // If needed then close the order
     let closeResult = await handleCloseTrend({ ...candleData, prevTrade, trendCatcherShift });
-    
+
     if (!closeResult || !isPrimaryTrade.status || !trendCatcherShift) {
         return false;
     }
@@ -220,10 +556,10 @@ async function handleTradeWithExistingTrade(isPrimaryTrade, trendCatcherShift, p
 async function handleCloseTrend(candleData) {
     try {
 
-        let { prevTrade , trendCatcherShift } = candleData;
-        
+        let { prevTrade, trendCatcherShift } = candleData;
+
         // Check order close resons
-        let reson = handleTradeCloseResons({...candleData.data , prevTrade , trendCatcherShift})
+        let reson = handleTradeCloseResons({ ...candleData.data, prevTrade, trendCatcherShift })
         let profitMargin = calculateTwoRangePercentage(prevTrade.entryPrice, candleData.close)
         if (reson) {
             telegram.sendMessage("Previous Tread will close,\n Your Profit would be: " + profitMargin + "% \n Resion\n" + reson);
@@ -256,7 +592,7 @@ async function handleCloseTrend(candleData) {
  * @returns 
  */
 function handleTradeCloseResons(candleData) {
-    let { prevTrade , smartTrailShift, trendCatcherShift, trendStrength } = candleData;
+    let { prevTrade, smartTrailShift, trendCatcherShift, trendStrength } = candleData;
     let prevTradeDirection = prevTrade.direction;
 
     let resons = null
